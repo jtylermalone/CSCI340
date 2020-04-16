47e8463b96e6bed34472da85765478afa5ff2604
Tyler <malonet@g.cofc.edu>
2020-04-16 13:29:47 -0400

    done with HW10

--- HW10/HW10.c
+++ HW10/HW10.c
@@ -1,66 +1,80 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-#include <math.h>
-
-
-int convert(char* hex) {
-
-    int decimal = 0;
-
-    int length = strlen(hex);
-
-    for (int i = 0; i < length; i++) {
-
-        char digit = hex[length - 1 - i];
-
-        if (digit >= '0' && digit <= '9')
-            decimal += (digit - '0') * pow(16, i);
-        else
-            decimal += (digit - 'A' + 10) * pow(16, i);
-
-    }
-
-    return decimal;
-
-}
 
 int main(int argc, char *argv[]) {
+
 
     if (argc != 2) {
         printf("Usage: ./HW10 <path_to_file>\n");
         exit(1);
     }
-    int l;
-    int n;
+
+    // opening file for reading
     int file = open(argv[1], O_RDONLY);
-    char *offset_array_size = malloc(4);
-    n = read(file, offset_array_size, 4);
-    printf("**** 1st 4 bytes: %d\n", *offset_array_size);
-    // dividing by 4 here because we're dealing with bytes
-    *offset_array_size = *offset_array_size / 4;
-    printf("offset array size: %d\n", *offset_array_size);
-    int *offset_array = malloc(*offset_array_size);
-    for (int i = 0; i < *offset_array_size; i++) {
-        int *offset = malloc(4);
-        read(file, offset, 4);
-        offset_array[i] = *offset;
-    }
 
-    for (int i = 0; i < *offset_array_size; i++)
-        printf("%d\n", offset_array[i]);
-    printf("\n\n\n");
+    // getting file signature... we don't really
+    // do anything with this, we just need a place
+    // to dispose of the file signature
+    char *file_signature = malloc(4);
+
+    // reading 4 bytes of the file signature
+    read(file, file_signature, 4);
+
+    // freeing it here... if for some reason you needed
+    // the file signature elsewhere, you coulld move this
+    // line to the end of the program
+    free(file_signature);
+
+    // the next 4 bytes of the file contains the length
+    // of the offset array.
+    int *offset_array_length = malloc(4);
+    read(file, offset_array_length, 4);
+
+    // allocating space for offset array
+    int *offset_array = malloc(sizeof(int) * *offset_array_length);
+
+    // this loop loads values into the offset array
+    for (int i = 0; i < *offset_array_length; i++) {
+
+        // allocating space for offset array element,
+        // reading it in and putting it in the array
+        char *line_location = malloc(4);
+        read(file, line_location, 4);
+        offset_array[i] = *line_location;
+    }
     
-    for (int i = 0; i < *offset_array_size; i++) {
-        lseek(file, offset_array[i], SEEK_SET);
-        int *line_length;
-        read(file, line_length, 4);
-        printf("%d\n", *line_length);
+    // this loop goes through the file and prints 
+    // each line.
+    for (int i = 0; i < *offset_array_length; i++) {
 
+        // lseek moves the file pointer to the specified
+        // offset of bytes. setting the last paramter to
+        // SEEK_SET means that lseek will offset from the 
+        // start of the file
+        lseek(file, offset_array[i], SEEK_SET);
+        int *line_length = malloc(4);
+        read(file, line_length, 4);
+
+        // this loop actually prints each line. it runs
+        // for line_length - 4 because the first 4 bytes
+        // of each line define the length of the line. we 
+        // don't want to print those characters
+        for (int i = 0; i < (*line_length -  4); i++) {
+            char *letter = malloc(1);
+            read(file, letter, 1);
+            printf("%s", letter);
+            free(letter); 
+        }
+
+        free(line_length);
     }
+
+    free(offset_array_length);
+    free(offset_array);
+    close(file);
     
 
 }
